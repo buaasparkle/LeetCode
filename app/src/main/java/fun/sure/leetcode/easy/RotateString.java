4d72af0ee31f3c4b5ca47b19515f7fb4d5099310
wangshuo <chuanqianyulan@163.com>
2018-03-24 12:54:51 +0800

    ADD: [easy] Rotate String

--- app/src/main/java/fun/sure/leetcode/easy/RotateString.java
+++ app/src/main/java/fun/sure/leetcode/easy/RotateString.java
@@ -5,4 +5,48 @@ package fun.sure.leetcode.easy;
  */
 
 public class RotateString {
+
+    /**
+     * We are given two strings, A and B.
+
+     A shift on A consists of taking string A and moving the leftmost character to the rightmost position. For example, if A = 'abcde', then it will be 'bcdea' after one shift on A. Return True if and only if A can become B after some number of shifts on A.
+
+     Example 1:
+     Input: A = 'abcde', B = 'cdeab'
+     Output: true
+
+     Example 2:
+     Input: A = 'abcde', B = 'abced'
+     Output: false
+     Note:
+
+     A and B will have length at most 100.
+     */
+
+    // [PASS]
+    public boolean rotateString(String A, String B) {
+        if (A == null && B == null) {
+            return true;
+        } else if (A == null || B == null) {
+            return false;
+        }
+        if (A.length() == 0 && B.length() == 0) {
+            return true;
+        }
+        if (A.length() != B.length()) {
+            return false;
+        }
+        char firstCharInB = B.charAt(0);
+        char charInA;
+        for (int i = 0; i < A.length(); i++) {
+            charInA = A.charAt(i);
+            if (charInA == firstCharInB) {
+                String rotatedA = String.format("%s%s", A.substring(i), A.substring(0, i));
+                if (B.equals(rotatedA)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
