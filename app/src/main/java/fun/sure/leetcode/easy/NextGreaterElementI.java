95c95aa9b125829b8987a559d74879f62d4eb29f
wangshuo <chuanqianyulan@163.com>
2018-07-21 17:16:11 +0800

    ADD: [easy] Next Greater Element I

---
+++ app/src/main/java/fun/sure/leetcode/easy/NextGreaterElementI.java
@@ -0,0 +1,55 @@
+package fun.sure.leetcode.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by sure on 2018/7/21.
+ */
+
+public class NextGreaterElementI {
+
+    /**
+     * You are given two arrays (without duplicates) nums1 and nums2 where nums1’s elements are subset of nums2. Find all the next greater numbers for nums1's elements in the corresponding places of nums2.
+
+     The Next Greater Number of a number x in nums1 is the first greater number to its right in nums2. If it does not exist, output -1 for this number.
+
+     Example 1:
+     Input: nums1 = [4,1,2], nums2 = [1,3,4,2].
+     Output: [-1,3,-1]
+     Explanation:
+     For number 4 in the first array, you cannot find the next greater number for it in the second array, so output -1.
+     For number 1 in the first array, the next greater number for it in the second array is 3.
+     For number 2 in the first array, there is no next greater number for it in the second array, so output -1.
+     Example 2:
+     Input: nums1 = [2,4], nums2 = [1,2,3,4].
+     Output: [3,-1]
+     Explanation:
+     For number 2 in the first array, the next greater number for it in the second array is 3.
+     For number 4 in the first array, there is no next greater number for it in the second array, so output -1.
+     Note:
+     All elements in nums1 and nums2 are unique.
+     The length of both nums1 and nums2 would not exceed 1000.
+     */
+
+    // [ACCEPTED]
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> num2Map = new HashMap<>();
+        for (int i = 0; i < nums2.length; i++) {
+            num2Map.put(nums2[i], i);
+        }
+        int len = nums1.length;
+        int[] ret = new int[len];
+        for (int i = 0; i < nums1.length; i++) {
+            int index = num2Map.get(nums1[i]);
+            ret[i] = -1;
+            for (int j = index + 1; j < nums2.length; j++) {
+                if (nums2[j] > nums1[i]) {
+                    ret[i] = nums2[j];
+                    break;
+                }
+            }
+        }
+        return ret;
+    }
+}
